96baaf2f86c458f4e4854e8097dedbc77cd979bb
adarshpk1920 <116722793+adarshpk1920@users.noreply.github.com>
2022-11-30 14:30:43 +0530

    Add files via upload

---
+++ 20removeeven.py
@@ -0,0 +1,6 @@
+evenList=[22,34,77,88,4000]
+print("evenList items=",evenList)
+for ev in evenList:
+    if(ev % 2 == 0):
+        evenList.remove(ev)
+print("list after removing even number",evenList)
