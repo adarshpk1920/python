94588c803f0cc8bd468693b2952571a6e3cf99c2
adarshpk1920 <116722793+adarshpk1920@users.noreply.github.com>
2023-01-11 23:55:51 +0530

    Add files via upload

---
+++ co1/10.py
@@ -0,0 +1,4 @@
+#areaofacircle
+from math import pi
+radius = float(input("Enter radius of Circle : "))
+print("Area of the circle is : ",pi*radius*radius)
---
+++ co1/19.py
@@ -0,0 +1,8 @@
+#gcdof2number
+a = int(input("Type the first number : "))
+b = int(input("Type the second number : "))
+while b != 0:
+    r = a % b
+    a = b
+    b = r
+print("The GCD of the numbers is", a)
